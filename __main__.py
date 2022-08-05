13a8ad21721d782a7e1b7477ee682c8d59b1cb72
Bruno Muniz Perrut <bruno.devops@alterdata.com.br>
2022-08-04 22:13:14 -0300

    POC working

--- __main__.py
+++ __main__.py
@@ -1,28 +1,16 @@
-from ast import arg
-import multiprocessing
-from time import sleep
 from src.core.server import WServerCore
 from src.classes import oscheck
+
+import multiprocessing
+from time import sleep
+
 import threading
-core = WServerCore
+
+core = WServerCore()
 
 # core.initialize_socket_and_server()
 if __name__ == '__main__':
     loop = True
-    _results = []
     while loop:
-        # t = threading.Thread(name='webpage handler',
-        #                      target=core.search_for_page, args=["page.html", ])
-        t = threading.Thread(name="webserver",
-                             target=core.initialize_socket_and_server, args=[])
-        t.start()
-
+        core.initialize_socket_and_server()
         loop = False
-
-    #p = Process(target=core.search_for_page, args=(queue, 'teste3.html'))
-
-    # multiprocessing.freeze_support()
-    # with multiprocessing.Pool() as pool:
-    #print(pool.map(core.search_for_page, "teste.html"))
-
-    # print(retorno)
--- src/core/server.py
+++ src/core/server.py
@@ -1,72 +1,88 @@
-from math import sin
+from concurrent.futures import thread
+from threading import Thread
+
 import os
 import socket
-import json
 
 from src.classes.oscheck import OSCheck
 from http import HTTPStatus
 
 
 class WServerCore:
-    sock_addr = ('', 3010)
+    def __init__(self, port_num=3010):
+        self.sock_addr = ('', 3010)
 
-    lis_sock = (socket.AF_INET, socket.SOCK_STREAM)
+        self.lis_sock = (socket.AF_INET, socket.SOCK_STREAM)
 
-    def search_for_page(page_name: str):
+    def search_for_page(self, page_name: str):
         oper_sys = OSCheck.check_os()
+        files_list = []
 
         oper_sys_path = OSCheck.build_path(oper_sys)
 
         for root, dir, files in os.walk(oper_sys_path):
             for file in files:
+                files_list.append(file)
                 if page_name == file:
                     status = HTTPStatus(200)
-                    # return print(status, status.phrase)
                     f = open(f'{oper_sys_path}/{page_name}', 'r')
                     raw_file_data = f.readlines()
 
                     single_str_data = " ".join(raw_file_data)
                     return str(single_str_data)
-                else:
-                    status = HTTPStatus(404)
-                    return print(status, status.phrase)
 
-    def initialize_socket_and_server():
-        with socket.socket(WServerCore.lis_sock[0], WServerCore.lis_sock[1]) as soc:
+        if page_name not in files:
+            status = HTTPStatus(404)
+            f = open(f'{oper_sys_path}/notfound.html', 'r')
+            raw_file_data = f.readlines()
+
+            single_str_data = " ".join(raw_file_data)
+            return str(single_str_data)
+
+    def initialize_socket_and_server(self):
+        with socket.socket(self.lis_sock[0], self.lis_sock[1]) as soc:
             soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            soc.bind(WServerCore.sock_addr)
-            print(f'LISTENING ON PORT {WServerCore.sock_addr[1]}')
+            soc.bind(self.sock_addr)
+            print(f'LISTENING ON PORT {self.sock_addr[1]}')
             soc.listen(1)
-
             while True:
-                try:
+                thread = Thread(target=self.handle_connection, args=(soc,))
+                thread.start()
 
-                    conn, addr = soc.accept()
-                    req_data = conn.recv(1024)
-
-                    # print(req_data.decode('utf-8'))
-                    http_response_header = b"""\
+    def handle_connection(self, soc):
+        try:
+            conn, addr = soc.accept()
+            req_data = conn.recv(2048)
+            decoded_req = req_data.decode('utf-8')
+            http_response_header = b"""\
 HTTP/1.1 200 OK
 
 """
 
-                    # http_response_body = bytes("done", "utf8")
+            cut_decoded_req = decoded_req[4:19].split()
+            list_cut_decoded_req = cut_decoded_req[0].split()
+            url_path = list_cut_decoded_req[0]
+            url_path = url_path.split()[0]
+            url_path = url_path[1:]
 
-                    get_decoded_file_str = WServerCore.search_for_page(
-                        'page.html')
+            if "/favicon.ico" in url_path:
+                return
 
-                    http_response_body = bytes(get_decoded_file_str, "utf8")
-                    message = http_response_header + http_response_body
-                    # # print(http_response)
-                    # print(http_response_header)
-                    # print(http_response_body)
-                    conn.sendall(message)
-                    conn.close()
+            if ".html" not in url_path:
+                url_path = url_path + ".html"
 
-                except Exception:
-                    conn.close()
-                    soc.accept()
+            get_decoded_file_str = self.search_for_page(
+                url_path)
 
-                finally:
-                    conn.close()
-                    soc.accept()
+            http_response_body = bytes(get_decoded_file_str, "utf8")
+            message = http_response_header + http_response_body
+            conn.sendall(message)
+            conn.close()
+
+        except Exception:
+            conn.close()
+            soc.accept()
+
+        finally:
+            conn.close()
+            soc.accept()
